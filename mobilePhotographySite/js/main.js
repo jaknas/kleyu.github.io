d9683fac14f6d0a63c78679f628ae7ff28b0c5a2
Kuba Naskręski <36169811+kleyu@users.noreply.github.com>
2018-06-02 20:39:34 +0200

    add mobile-first website
    added website fo

---
+++ mobilePhotographySite/js/main.js
@@ -0,0 +1,12 @@
+// animate smoothscroll
+
+$("#view-work").on("click", function() {
+	const images = $("#images").position().top;
+
+	$("html,body").animate(
+		{
+			scrollTop: images
+		},
+		900
+	);
+});
